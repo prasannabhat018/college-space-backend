4ecf987908867f4d58a14b4f37f9f03065bec04d
prasannabhat018 <prasannabhat018@gmail.com>
2021-07-17 21:19:35 +0530

    testing db connection

--- startup/db.js
+++ startup/db.js
@@ -6,113 +6,81 @@ var user = process.env.MYSQL_USER;
 var password = process.env.MYSQL_PASSWORD;
 var database = process.env.MYSQL_DB;
 
-let dbConfig = {
-  "host": host,
-  "user": user,
-  "password": password,
-  "database": password,
-  "connectTimeout" : 60000
-};
 
-let pool = mysql.createPool(dbConfig);
+let mysqlDB = null; // db handler
+let connected = null; // default null / boolean
+let connectFreq = 1000; // When database is disconnected, how often to attempt reconnect? Miliseconds
+let testFreq = 10000;
 
-pool.on('connection', function (_conn) {
-    if (_conn) {
-        logger.info('Connected the database via threadId %d!!', _conn.threadId);
-        _conn.query('SET SESSION auto_increment_increment=1');
+function attemptMySQLConnection(callback) {
+    // console.log(process.env.MYSQL_HOST);
+    if (host && user && database) {
+  
+      mysqlDB = mysql.createPool({
+        host: host,
+        user: user,
+        password: password,
+        database: database,
+        connectionLimit: 300,
+        waitForConnections: true, // Default value.
+        queueLimit: 300, // Unlimited
+        acquireTimeout: 60000,
+        timeout: 60000,
+        debug: false
+      });
+  
+      testConnection((result) => {
+        callback(result)
+      })
+  
+    } else {
+    //   console.error('Check env variables: MYSQL_HOST, MYSQL_USER & MYSQL_DB')
+      callback(false)
     }
-});
-
-// const db= mysql.createConnection({
-//   host:host,
-//   user:user,
-//   password:password,
-//   database:database,
-// })
-
-// db.connect(function(err){
-//   if(err)
-//       return console.log(err)
-// })
-
-module.exports=pool;
-
-
-// let mysqlDB = null; // db handler
-// let connected = null; // default null / boolean
-// let connectFreq = 1000; // When database is disconnected, how often to attempt reconnect? Miliseconds
-// let testFreq = 10000;
-
-// function attemptMySQLConnection(callback) {
-//     // console.log(process.env.MYSQL_HOST);
-//     if (host && user && database) {
+  }
   
-//       mysqlDB = mysql.createPool({
-//         host: host,
-//         user: user,
-//         password: password,
-//         database: database,
-//         connectionLimit: 300,
-//         waitForConnections: true, // Default value.
-//         queueLimit: 300, // Unlimited
-//         acquireTimeout: 60000,
-//         timeout: 60000,
-//         debug: false,
-//         ssl: { rejectUnauthorized: false }
-//       });
+  function testConnection(cb) {
+    // console.log('testConnection')
+    mysqlDB.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
+      try {
+        if (error) {
+          throw new Error('No DB Connection');
+        } else {
+          if (results[0].solution) {
+            cb(true)
+          } else {
+            cb(false)
+          }
+        }
+      } catch (e) {
+        // console.error(e.name + ': ' + e.message);
+        cb(false)
+      }
+    });
+  }
   
-//       testConnection((result) => {
-//         callback(result)
-//       })
+  function callbackCheckLogic(res) {
+    if (res) {
+    //   console.log('Connect was good. Scheduling next test for ', testFreq, 'ms')
+      setTimeout(testConnectionCB, testFreq);
+    } else {
+    //   console.log('Connection was bad. Scheduling connection attempt for ', connectFreq, 'ms')
+      setTimeout(connectMySQL, connectFreq);
+    }
+  }
   
-//     } else {
-//     //   console.error('Check env variables: MYSQL_HOST, MYSQL_USER & MYSQL_DB')
-//       callback(false)
-//     }
-//   }
+  function testConnectionCB() {
+    testConnection((result) => {
+      callbackCheckLogic(result);
+    })
+  }
   
-//   function testConnection(cb) {
-//     // console.log('testConnection')
-//     mysqlDB.query('SELECT 1 + 1 AS solution', (error, results, fields) => {
-//       try {
-//         if (error) {
-//           throw new Error('No DB Connection');
-//         } else {
-//           if (results[0].solution) {
-//             cb(true)
-//           } else {
-//             cb(false)
-//           }
-//         }
-//       } catch (e) {
-//         // console.error(e.name + ': ' + e.message);
-//         cb(false)
-//       }
-//     });
-//   }
+  function connectMySQL() {
+    attemptMySQLConnection(result => {
+      callbackCheckLogic(result);
+    });
+  }
   
-//   function callbackCheckLogic(res) {
-//     if (res) {
-//     //   console.log('Connect was good. Scheduling next test for ', testFreq, 'ms')
-//       setTimeout(testConnectionCB, testFreq);
-//     } else {
-//     //   console.log('Connection was bad. Scheduling connection attempt for ', connectFreq, 'ms')
-//       setTimeout(connectMySQL, connectFreq);
-//     }
-//   }
+  connectMySQL(); // Start the process by calling this once
   
-//   function testConnectionCB() {
-//     testConnection((result) => {
-//       callbackCheckLogic(result);
-//     })
-//   }
-  
-//   function connectMySQL() {
-//     attemptMySQLConnection(result => {
-//       callbackCheckLogic(result);
-//     });
-//   }
-  
-//   connectMySQL(); // Start the process by calling this once
-  
-  // module.exports = mysqlDB;
+  module.exports = mysqlDB;
